c7a8b5961e0a5dbc45542e5f43cecdb52e25ec9d
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-08-22 23:25:18 -0300

    chore: customize CSS Modules scoped name

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		modules: {
+			generateScopedName: '[local]_[hash:base64:5]',
+		},
+	},
 });

8eb53e7f9c23dcd8293e5974c904d03aff45f4ef
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2025-09-14 14:16:43 -0300

    docs: create basic preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { getDefaultItems } from './fixtures';
+import { SortableList, sortItems, removeItem } from './lib';
+import '@fontsource/rubik/400.css';
 import './App.css';
+import './lib/styles.css';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [items, setItems] = useState(getDefaultItems(5));
+
+	const handleDrop = (e: (typeof SortableList.Events)['ondrop']) => {
+		const { draggedItemIndex, isBetweenBounds, canRemoveOnDropOut } = e;
+		if (!isBetweenBounds && canRemoveOnDropOut) setItems(removeItem(items, draggedItemIndex));
+	};
+
+	const handleDragEnd = (e: (typeof SortableList.Events)['ondragend']) => {
+		const { draggedItemIndex, targetItemIndex, isCanceled } = e;
+		if (!isCanceled && typeof targetItemIndex === 'number' && draggedItemIndex !== targetItemIndex)
+			setItems(sortItems(items, draggedItemIndex, targetItemIndex));
+	};
 
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+		<SortableList onDrop={handleDrop} onDragEnd={handleDragEnd}>
+			{items.map((item, index) => (
+				<SortableList.Item key={item.id} {...item} index={index}>
+					<div className="rsl-item-content">
+						<span className="rsl-item-content__text">{String(item.text)}</span>
+					</div>
+				</SortableList.Item>
+			))}
+		</SortableList>
 	);
 }
 
---
+++ src/fixtures.ts
@@ -0,0 +1,93 @@
+import { SortableList } from './lib';
+
+export const defaultRootProps: typeof SortableList.Props = {
+	gap: 12,
+	direction: 'vertical',
+	transition: {
+		duration: 320,
+		easing: 'cubic-bezier(0.2, 1, 0.1, 1)',
+	},
+	hasWrapping: false,
+	hasLockedAxis: false,
+	hasBoundaries: false,
+	canClearOnDragOut: false,
+	canRemoveOnDropOut: false,
+	isLocked: false,
+	isDisabled: false,
+};
+
+export function getDefaultItems(length: number): (typeof SortableList.ItemData)[] {
+	return Array.from({ length }, (_, i) => ({
+		id: `list-item-${i + 1}`,
+		text: `List Item ${i + 1}`,
+	}));
+}
+
+export function getVaryingItems(length: number): (typeof SortableList.ItemData)[] {
+	return Array.from({ length }, (_, i) => {
+		const number = i + 1;
+		const text =
+			number % 5 === 0
+				? `List Item ${number} with a bit more of content that will break into more multiple lines`
+				: String(number / 5).split('.')[1] === '4'
+					? `List Item ${number} that will break into multiple lines`
+					: `List Item ${number}`;
+		return {
+			id: `list-item-${number}`,
+			text,
+		};
+	});
+}
+
+export function getLockedItems(length: number): (typeof SortableList.ItemData)[] {
+	return Array.from({ length }, (_, i) => ({
+		id: `locked-item-${i + 1}`,
+		text: `Locked Item ${i + 1}`,
+		isLocked: true,
+	}));
+}
+
+export function getDisabledItems(length: number): (typeof SortableList.ItemData)[] {
+	return Array.from({ length }, (_, i) => ({
+		id: `disabled-item-${i + 1}`,
+		text: `Disabled Item ${i + 1}`,
+		isDisabled: true,
+	}));
+}
+
+export function getInteractiveItems(): (typeof SortableList.ItemData)[] {
+	return [
+		{
+			id: 'list-item-1',
+			text: 'List Item 1',
+			type: 'input',
+			value: 'Input field',
+		},
+		{
+			id: 'list-item-2',
+			text: 'List Item 2',
+			type: 'textarea',
+			value: 'Textarea field',
+		},
+		{
+			id: 'list-item-3',
+			text: 'List Item 3',
+			type: 'select',
+		},
+		{
+			id: 'list-item-4',
+			text: 'List Item 4',
+			type: 'checkbox',
+		},
+		{
+			id: 'list-item-5',
+			text: 'List Item 5',
+			type: 'button',
+		},
+		{
+			id: 'list-item-6',
+			text: 'List Item 6',
+			type: 'a',
+		},
+	];
+}
